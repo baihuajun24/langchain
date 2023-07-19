9167a6f66c019243d2e57e1678064feff38eedc9
Huajun Bai <baihuajun24@gmail.com>
2023-07-19 16:24:01 +0800

    update openai api key

--- few-shot-test.py
+++ few-shot-test.py
@@ -5,10 +5,50 @@ from langchain.prompts.example_selector.semantic_similarity import SemanticSimil
 from langchain.vectorstores import Chroma
 from langchain import OpenAI, SQLDatabase, SQLDatabaseChain
 from langchain.llms.glm import GLM
-from langchain.llms import OpenLLM
 import yaml
 from flask import Flask, request, jsonify
 import argparse
+from typing import Dict
+
+def read_key_from_file(filename):
+    with open(filename, 'r') as file:
+        key = file.readline().strip()  # read the first line and remove newline character
+    return key
+
+def _parse_example(result: Dict) -> Dict:
+    sql_cmd_key = "sql_cmd"
+    sql_result_key = "sql_result"
+    table_info_key = "table_info"
+    input_key = "input"
+    final_answer_key = "answer"
+
+    _example = {
+        "input": result.get("query"),
+    }
+
+    steps = result.get("intermediate_steps")
+    answer_key = sql_cmd_key # the first one
+    for step in steps:
+        # The steps are in pairs, a dict (input) followed by a string (output).
+        # Unfortunately there is no schema but you can look at the input key of the
+        # dict to see what the output is supposed to be
+        if isinstance(step, dict):
+            # Grab the table info from input dicts in the intermediate steps once
+            if table_info_key not in _example:
+                _example[table_info_key] = step.get(table_info_key)
+
+            if input_key in step:
+                if step[input_key].endswith("SQLQuery:"):
+                    answer_key = sql_cmd_key # this is the SQL generation input
+                if step[input_key].endswith("Answer:"):
+                    answer_key = final_answer_key # this is the final answer input
+            elif sql_cmd_key in step:
+                _example[sql_cmd_key] = step[sql_cmd_key]
+                answer_key = sql_result_key # this is SQL execution input
+        elif isinstance(step, str):
+            # The preceding element should have set the answer_key
+            _example[answer_key] = step
+    return _example
 
 YAML_EXAMPLES = """
 - input: 今天是2023-06-28，昨天互动量最高的微博账号是?
@@ -145,23 +185,41 @@ example_selector = SemanticSimilarityExampleSelector.from_examples(
                         # This is the VectorStore class that is used to store the embeddings and do a similarity search over.
                         Chroma,  # type: ignore
                         # This is the number of examples to produce and include per prompt
-                        k=min(3, len(examples_dict)),
+                        k=min(2, len(examples_dict)),
                     )
 
 few_shot_prompt = FewShotPromptTemplate(
     example_selector=example_selector,
     example_prompt=example_prompt,
-    prefix=_sqlite_prompt + "Here are some examples:",
+    prefix=_sqlite_prompt + "Don't use \_ in sql. Here are some examples:",
     suffix=PROMPT_SUFFIX,
     input_variables=["table_info", "input", "top_k"],
 )
 
-db = SQLDatabase.from_uri("sqlite:///D:/huajun/softwares/litestream-0.3.9/data_0706.db")
-# local_llm = OpenAI(temperature=0, openai_api_key='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')
+my_table_info = {
+    "social_reaction": """CREATE TABLE social_reaction (
+        the_date Date NOT NULL,
+        type TEXT NOT NULL,
+        name TEXT NOT NULL,
+        content TEXT NOT NULL,
+        url TEXT NOT NULL,
+        reaction INTEGER NOT NULL,
+        language TEXT,
+        programe TEXT,
+        plate_name TEXT
+    )
+)
+"""
+}
+
+db = SQLDatabase.from_uri("sqlite:///D:/huajun/softwares/litestream-0.3.9/data_0706.db", include_tables=['social_reaction'], custom_table_info=my_table_info)
+key_file = 'key.txt'  # Replace with the actual path to your text file
+api_key = read_key_from_file(key_file)
+local_llm = OpenAI(temperature=0, openai_api_key=api_key)
 # local_llm = GLM()
 # local_llm.load_model(model_name_or_path='D:\\huajun\\chatGLM\\chatGLM\\chatglm2-6b')
-local_llm = OpenLLM(model_name="chatglm", model_id='D:\\huajun\\chatGLM\\chatGLM\\chatglm2-6b')
-local_llm("What is the difference between a duck and a goose? And why there are so many Goose in Canada?")
+# local_llm = OpenLLM(model_name="chatglm", model_id='D:\\huajun\\chatGLM\\chatGLM\\chatglm2-6b')
+# local_llm("What is the difference between a duck and a goose? And why there are so many Goose in Canada?")
 # local_chain = SQLDatabaseChain.from_llm(local_llm, db, prompt=few_shot_prompt, use_query_checker=True, verbose=True, return_intermediate_steps=True)
 # Test without checker for local llm
 local_chain = SQLDatabaseChain.from_llm(local_llm, db, prompt=few_shot_prompt, verbose=True, return_intermediate_steps=True)
@@ -188,8 +246,12 @@ def run_tests():
         "2023-06-28这一天总台互动量最低的微博账号是？",
         #... add more queries for testing
     ]
+    result = ''
     for query in queries:
-        result = local_chain(query)
+        try:
+          result += local_chain(query)
+        except:
+          result += "Error occurred\n"
         print(result)
 
 if __name__ == '__main__':
@@ -203,11 +265,20 @@ if __name__ == '__main__':
                 data = request.get_json()
                 query = data['query']
                 result = local_chain(query)
+                print("*** Query succeeded")
+                example = _parse_example(result)
                 return jsonify({'result': str(result)})
             except Exception as e:
-                print(f"Error occurred: {str(e)}")  # Log the error
-                return jsonify({'error': 'An error occurred processing the request.'}), 500
-
+                  print("*** Query failed")
+                  result = {
+                      "query": query,
+                      "intermediate_steps": e.intermediate_steps
+                  }
+                  example = _parse_example(result)
+                  print(f"Error occurred: {str(e)}")  # Log the error
+                  return jsonify({'error': 'An error occurred processing the request.'}), 500
+            yaml_example = yaml.dump(example, allow_unicode=True)
+            print("\n" + yaml_example)
         app.run(host='0.0.0.0', port=5000)
     else:
         # Run tests
